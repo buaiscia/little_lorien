def10a932d29f92d12a28fc413373c61a25f910c
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2020-01-20 15:53:29 +0100

    add landing component

--- src/HOC/Layout/Layout.jsx
+++ src/HOC/Layout/Layout.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Suspense } from 'react-router-dom';
+
+import Landing from '../../Components/Landing';
 
 class Layout extends Component {
-    render () {
+
+   
+
+    render() {
+
+        const landing = null;
+        
         return (
-            <React.Fragment>
-                {this.props.children}
-            </React.Fragment>
+            <BrowserRouter>
+                <React.Fragment>
+                    <h1>Layout page</h1>
+                    
+                    <Landing />
+                </React.Fragment>
+            </BrowserRouter>
+
         )
     }
 }
