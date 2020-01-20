3eff81c935c4bde5b51b49124b63907cee0b04d9
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2020-01-20 15:26:27 +0100

    create HOC layout comp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import Layout from './HOC/Layout/Layout'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Layout />
+    );
+  }
+
+
 }
 
 export default App;
