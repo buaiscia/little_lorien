3a235d9f777662e85617bd4e6f4f8985b4952980
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2020-03-23 11:13:39 +0100

    refactored structure + migrated layout into appjs + browserrouter to root + changed naming convention

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import Layout from './HOC/Layout/Layout'
+import Landing from './pages/landing/landing';
+
+import About from './pages/about/about';
+import Contact from './components/contact/contact';
 
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+  }
+
   render() {
     return (
-      <Layout />
+      <div>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={Landing}
+          />
+          <Route
+            path="/about"
+            component={About}
+          />
+          <Route
+            path="/contact"
+            component={Contact}
+          />
+        </Switch>
+      </div>
     );
   }
 
